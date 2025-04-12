64113a5f65e3be4ef9fc2949f8068996a14bc216
sheikh-abdullah-al-alvi <sheikhabdullahalalvi221@gmail.com>
2025-04-12 14:01:30 +0600

    visiting button optimite done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './Country.css'
+const Country = ({ country }) => {
+    const { name, flags } = country;
 
-const Country = ({country}) => {
-const {name,flags}=country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
+
     return (
         <div className="country">
             <h4>Name:{name.common}</h4>
+
             <img className='image' src={flags.png} alt="" />
+            <div className='country'>
+
+                <button onClick={handleVisited}>{visited?'Going':'visited'}</button> <br /><br />
+                {visited && 'I have Visited The country'}
+
+            </div>
+
+
+
         </div>
     );
 };
